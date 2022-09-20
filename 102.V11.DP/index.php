5c434ff3bd59e525e4dc2ad3f99b0e80dce456ed
Dhiraj Pudasaini <113920831+Pudasainidhiraj@users.noreply.github.com>
2022-09-20 14:40:40 +0545

    Add files via upload

---
+++ 102.V11.DP/index.php
@@ -0,0 +1,76 @@
+<?php require_once('./includes/header.php');?>
+
+<div class="container ">
+	<div class="col-md-10 offset-md-1">
+		<h6 class=" px-2 mt-2">Advertorial</h6>
+		<hr>
+		
+		<div style="font-family: 'Source Serif Pro', serif; color:#2528a2;">
+			<h1 class="text-center ">Ivy League Economist's Hypothesis On Currency</h1>
+		</div>
+
+		<div>
+			<div >
+			<h5 class="  p-2">Article Summary</h5>
+		</div>
+
+
+		<div class="row border my-2 py-2" style="box-shadow: 0 5px 5px rgba(0,0,0,0.3);">
+			<div class="col-md-6">
+				<img src="assets/images/Stephen Roach.jpg" class="img-fluid   mt-3 ">
+				
+				    <a class="btn-style-5 mt-2" href="https://secure.stansberryinnovationsreport.com/?cid=MKT421756&eid=MKT427110&encryptedSnaid=&snaid=&step=start&affId=1587&s1=102fe315c3364e5a1da6d1ff885229&assetId=AST138671&page=1" style="background-color: #d31a1a"> Watch Video Now>> </a>
+				  
+			</div>
+			<div class="col-md-6 mt-3" style="font-size: 18px">
+				<p>Yale University Stephen Roach surprised the financial community when he wrote a story for Bloomberg titled…
+				</p>
+				<p>“How the Coming Crash in the Dollar Will Unfold.”</p>
+				<p>In the article, Roach says, “America’s saving and current-account problems are about to come into play with a vengeance.”</p>
+				<p>And Roach is not the only financial expert warning <a href="#"><u style="color: blue;"><b>Americans to pay attention to what’s happening to our currency</b></u></a></p>
+				<p>Porter Stansberry, who started one of America’s most successful financial research firm more than 20 years ago, says:</p>
+				<p >“Today, every savvy wealthy person I know is desperately seeking a way out of our corrupt and bankrupt global financial system and the U.S. dollar. And it’s fascinating how they’re doing it.”   <a href="https://secure.stansberryinnovationsreport.com/?cid=MKT421756&eid=MKT427110&encryptedSnaid=&snaid=&step=start&affId=1587&s1=102fe315c3364e5a1da6d1ff885229&assetId=AST138671&page=1"> <u style="color: blue;"><b>More here…</b></u></a>
+				</p>
+			</div>
+			
+		</div>
+
+
+				<div class="mt-4 px-3" style="font-size: 18px;">
+					<h2 style="font-family: 'Source Serif Pro', serif; color:#2528a2;">Stansberry adds:</h2>
+			<p>“What we’re ultimately looking at here will be the <a href="#"><u style="color: blue;"><b>Biggest transfer of wealth in the history of mankind</b></u></a> — bigger than any political revolution by a huge margin.”
+
+			<p> Stansberry is well known in the investment world for not only identifying many of the decade’s biggest winners (he recommended Amazon at $59 and Paypal before it soared 800%) but also many of the biggest busts, including the collapse of GM, Fannie Mae, Freddie Mac, and many others.</p>
+
+			<p>And today Stansberry says a huge currency shift could be underway.</p>
+
+			<p>That’s why Stansberry went on camera for 10 minutes from his corner office <a href="#"><u style="color: blue;"><b>(click here to watch) </b></u></a>to explain the full U.S. dollar story, and the 3 critical steps every American should now take.</p>
+
+			<p>I strongly encourage you to take a few minutes to get the facts for yourself</p>
+			<div class="col-lg-8 offset-lg-2">
+				<a class="btn-style-5" href="" style="background-color: #d31a1a"> Watch Video Now>></a>
+			</div>
+		</div>
+
+
+
+
+			
+
+
+		
+	
+
+			
+		
+				
+
+				
+
+
+
+
+
+<?php require_once('./includes/footer.php');?>
+ 
+
